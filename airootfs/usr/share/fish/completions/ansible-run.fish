90f967c47a831e309f3fd74322ec6b28cf750336
kasefuchs <kasefuchs@protonmail.com>
2026-07-15 15:10:50 +0300

    refactor: migrate to chezmoi & remove ansible

--- airootfs/usr/share/fish/completions/ansible-run.fish
+++
@@ -1,7 +0,0 @@
-function __ansible_run_playbooks
-    for file in /usr/share/ansible/playbooks/*.yaml
-        echo (basename $file .yaml)
-    end
-end
-
-complete -c ansible-run -f -a '(__ansible_run_playbooks)'
